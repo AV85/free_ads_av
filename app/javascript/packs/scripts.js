5fb899e854cbf536b08ee2e06a3f076e34fb2ad4
Alexandr <sasha85ua@gmail.com>
2020-06-13 17:38:27 +0300

    added ui to myaccount

--- app/javascript/packs/scripts.js
+++ app/javascript/packs/scripts.js
@@ -3,21 +3,4 @@ $(document).on('ready turbolinks:load', function() {
   $( "#phone-block-link" ).on( "click", function( event ) {
     hiddenBoxPhone.toggle('slow');
   });
-  // const hiddenBoxSubcategory1 = $( "#subcategories-list-1" );
-  // const hiddenBoxSubcategory2 = $( "#subcategories-list-2" );
-  // const hiddenBoxSubcategory3 = $( "#subcategories-list-3" );
-  // const hiddenBoxSubcategory4 = $( "#subcategories-list-4" );
-  //
-  // $("#subcategories-link-1" ).on( "click", function( event ) {
-  //   hiddenBoxSubcategory1.toggle('slow');
-  // });
-  // $("#subcategories-link-2" ).on( "click", function( event ) {
-  //   hiddenBoxSubcategory2.toggle('slow');
-  // });
-  // $("#subcategories-link-3" ).on( "click", function( event ) {
-  //   hiddenBoxSubcategory3.toggle('slow');
-  // });
-  // $("#subcategories-link-4" ).on( "click", function( event ) {
-  //   hiddenBoxSubcategory4.toggle('slow');
-  // });
 });
