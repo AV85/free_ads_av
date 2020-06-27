065511de8e27eef4b5bce3cb90b8bb21dec41495
Alexandr <sasha85ua@gmail.com>
2020-06-27 15:56:19 +0300

    added dashboard elements

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,5 +4,8 @@ class Admin::DashboardController < ApplicationController
     unless current_user.admin?
       redirect_to root_path, alert: 'У Вас недостаточно прав для посещения данной страницы'
     end
+    @categories = Category.all
+    @ads = Ad.all
+    @users = User.all
   end
 end
